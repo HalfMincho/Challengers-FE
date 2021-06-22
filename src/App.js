ac7d09a8fc59affc84abd6941a9130b6fd463ceb
Hyeonjin Jeong <73835184+HyeonDay@users.noreply.github.com>
2021-06-22 14:59:10 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Route path="/page" component={} exact />
+    </div>
+  );
+}
+
+export default App;
